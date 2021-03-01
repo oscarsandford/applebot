b9f46637e0e7f47f478c11963c9361d38601bdf3
Oscar Sandford <oscarsandford@gmail.com>
2021-02-28 23:08:31 -0800

    Small fix to convert to PST when Heroku returns UTC

--- src/functions/user.js
+++ src/functions/user.js
@@ -1,7 +1,9 @@
 module.exports = {
 	apple_response : function (message) {
 		let day = new Date;
-		let hour_of_day = day.getHours();
+		// We take away 8 here because Heroku uses UTC, so this gets us PST without messing with Heroku.
+		let hour_of_day = day.getHours() - 8;
+		console.log(hour_of_day);
 		if (hour_of_day > 17)
 			message.channel.send("Good evening, "+message.author.username+". :apple:");
 		else if (hour_of_day > 12)
