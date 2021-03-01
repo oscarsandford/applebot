fd6031473fddcf42c94d897cb1a22adf47d4ee88
Oscar Sandford <oscarsandford@gmail.com>
2021-02-28 23:16:16 -0800

    Revisited a naive fix!

--- src/functions/user.js
+++ src/functions/user.js
@@ -3,7 +3,8 @@ module.exports = {
 		let day = new Date;
 		// We take away 8 here because Heroku uses UTC, so this gets us PST without messing with Heroku.
 		let hour_of_day = day.getHours() - 8;
-		console.log(hour_of_day);
+		if (hour_of_day < 0) hour_of_day = 24 + hour_of_day;
+
 		if (hour_of_day > 17)
 			message.channel.send("Good evening, "+message.author.username+". :apple:");
 		else if (hour_of_day > 12)
